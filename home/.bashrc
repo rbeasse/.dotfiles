be96fa6fe3788c7779a1779bf243d2cf79d35367
Ryan Beasse <ryan@emergeknowledge.com>
2024-06-18 13:55:06 -0500

    First itteration of config
    Added bashrc
    Added .gitconfig
    Added startship
    
    Included some scripts for making the process easier

---
+++ home/.bashrc
@@ -0,0 +1,27 @@
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+cd ~
+
+if [ -f /usr/local/bin/eza ]; then
+  alias ls='eza -lh --group-directories-first'
+fi
+
+if [ -f /usr/local/bin/bat ]; then
+  alias cat='bat'
+fi
+
+if [ -f ~/.local/bin/zoxide ]; then
+  eval "$(zoxide init bash)"
+
+  alias cd='z'
+fi
+
+if [ -f /usr/local/bin/fzf ]; then
+  eval "$(fzf --bash)"
+fi
+
+if [ -f /usr/local/bin/starship ]; then
+  eval "$(starship init bash)"
+fi
---
+++ scripts/terminal.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# eza
+# https://github.com/eza-community/eza
+if [ -f /usr/local/bin/eza ]; then
+  echo 'eza is already installed'
+else
+  curl -sL https://github.com/eza-community/eza/releases/download/v0.18.17/eza_x86_64-unknown-linux-gnu.tar.gz | tar xz
+  sudo chmod +x eza
+  sudo chown root:root eza
+  sudo mv eza /usr/local/bin/eza
+fi
+
+# bat
+# https://github.com/sharkdp/bat
+if [ -f /usr/local/bin/bat ]; then
+  echo 'bat is already installed'
+else
+  curl -sL https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-x86_64-unknown-linux-gnu.tar.gz | tar xz
+  cd bat-v0.24.0-x86_64-unknown-linux-gnu/
+  sudo chmod +x bat
+  sudo chown root:root bat
+  sudo mv bat /usr/local/bin/bat
+  rm -rf ../bat-v0.24.0-x86_64-unknown-linux-gnu/
+fi
+
+# zoxide
+# https://github.com/ajeetdsouza/zoxide
+if [ -f ~/.local/bin/zoxide ]; then
+  echo 'zoxide is already installed'
+else
+  curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+fi
+
+# starship
+# https://github.com/starship/starship
+if [ -f /usr/local/bin/starship ]; then
+  echo 'starship is already installed'
+else
+  curl -sS https://starship.rs/install.sh | sh
+fi
+
+# fzf
+# https://github.com/junegunn/fzf
+if [ -f /usr/local/bin/fzf ]; then
+  echo 'fzf is already installed'
+else
+  curl -sL https://github.com/junegunn/fzf/releases/download/0.53.0/fzf-0.53.0-linux_amd64.tar.gz | tar xz
+  sudo chmod +x fzf
+  sudo chown root:root fzf
+  sudo mv fzf /usr/local/bin/fzf
+fi
