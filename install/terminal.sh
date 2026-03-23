9eab0e4b8cff82bef022366a72836b0b7fdae117
Ryan Beasse <git@ryanbeasse.com>
2026-03-22 20:25:53 -0500

    Update eza, bat, fzf to latest versions

--- install/terminal.sh
+++ install/terminal.sh
@@ -6,7 +6,7 @@ if [ -f /usr/local/bin/eza ]; then
   echo 'eza is already installed'
 else
   cd $HOME/.dotfiles
-  curl -sL https://github.com/eza-community/eza/releases/download/v0.18.17/eza_x86_64-unknown-linux-gnu.tar.gz | tar xz
+  curl -sL https://github.com/eza-community/eza/releases/download/v0.23.4/eza_x86_64-unknown-linux-gnu.tar.gz | tar xz
   sudo chmod +x eza
   sudo chown root:root eza
   sudo mv eza /usr/local/bin/eza
@@ -18,7 +18,7 @@ if [ -f /usr/local/bin/bat ]; then
   echo 'bat is already installed'
 else
   cd $HOME/.dotfiles
-  curl -sL https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-x86_64-unknown-linux-gnu.tar.gz | tar xz
+  curl -sL https://github.com/sharkdp/bat/releases/download/v0.26.1/bat-v0.26.1-x86_64-unknown-linux-gnu.tar.gz | tar xz
   cd bat-*/
   sudo chmod +x bat
   sudo chown root:root bat
@@ -32,7 +32,7 @@ if [ -f /usr/local/bin/fzf ]; then
   echo 'fzf is already installed'
 else
   cd $HOME/.dotfiles
-  curl -sL https://github.com/junegunn/fzf/releases/download/0.53.0/fzf-0.53.0-linux_amd64.tar.gz | tar xz
+  curl -sL https://github.com/junegunn/fzf/releases/download/v0.70.0/fzf-0.70.0-linux_amd64.tar.gz | tar xz
   sudo chmod +x fzf
   sudo chown root:root fzf
   sudo mv fzf /usr/local/bin/fzf
